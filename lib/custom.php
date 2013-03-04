af6637800efc2dfffa9aa87f6064be1a3a07e68a
poxtron <poxtron@facebook.com>
2013-03-03 23:35:44 -0600

    Update custom.php

--- lib/custom.php
+++ lib/custom.php
@@ -1,4 +1,8 @@
 <?php
-/**
- * Custom functions
- */
+
+function roots_body_class_msg($classes) { 	
+  if (current_theme_supports('bootstrap-top-navbar')) {  	
+    $classes[] = 'top-navbar';
+  }
+
+?>
